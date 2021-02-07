9506ca94f7b9a5c53640f260c059cb766cc2f72f
unknown <rafaeelsgamer@gmail.com>
2021-02-06 22:42:56 -0300

    Removed custom scrollbar

--- vue.config.js
+++ vue.config.js
@@ -4,21 +4,13 @@ module.exports = {
   lintOnSave: false,
   publicPath: "/asura-site",
   productionSourceMap: false,
-  chainWebpack: (config) => {
-    if (process.env.NODE_ENV === "production"){
-      config.plugin('html').tap((opts) => {
-        opts[0].filename = './404.html';
-        return opts;
-      });
-    }
-  },
   configureWebpack: {
     plugins: [
      {
       apply: (compiler) => {
-        compiler.hooks.afterEmit.tap('AfterEmitPlugin', (compilation) => {
-         const content = fs.readFileSync(path.join(__dirname, "./dist/404.html"), "utf-8")
-         fs.writeFileSync(path.join(__dirname, "./dist/index.html"), content)
+        compiler.hooks.afterEmit.tap('AfterEmitPlugin', () => {
+         const content = fs.readFileSync(path.join(__dirname, "./dist/index.html"), "utf-8")
+         fs.writeFileSync(path.join(__dirname, "./dist/404.html"), content)
         });
      }
     }
