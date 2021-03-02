6833d2cdc55021082713ff2604bd04be65582ea7
unknown <rafaeelsgamer@gmail.com>
2021-03-01 21:14:24 -0300

    Optimzed website

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import Buefy from 'buefy'
+import {Button, Modal, Carousel, Navbar} from 'buefy'
 import 'buefy/dist/buefy.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faChevronDown,faSun, faMoon  } from '@fortawesome/free-solid-svg-icons'
@@ -13,7 +13,11 @@ Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
-Vue.use(Buefy)
+Vue.use(Button)
+Vue.use(Modal)
+Vue.use(Carousel)
+Vue.use(Navbar)
+
 new Vue({
   router,
   render: h => h(App)
