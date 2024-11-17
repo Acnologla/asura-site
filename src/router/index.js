fc72b2c81a8e1e1ebb05f3886e311852fd93f275
rafael-jurisoft <rafael@jurisoft.com.br>
2024-11-17 12:04:54 -0300

    Added privacy and terms

--- src/router/index.js
+++ src/router/index.js
@@ -1,61 +1,72 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Galos from "../views/Galos"
-import Home from '../views/Home'
-import Donate from "../views/Donate"
-import Commands from "../views/Commands"
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Galos from "../views/Galos";
+import Home from "../views/Home";
+import Donate from "../views/Donate";
+import Commands from "../views/Commands";
 
-
-const RinhaTutorial = () => import("../views/RinhaTutorial")
-const RinhaInfo = () => import("../views/RinhaInfo")
-const CreateGalo = () => import("../views/CreateGalo")
-Vue.use(VueRouter)
+const RinhaTutorial = () => import("../views/RinhaTutorial");
+const RinhaInfo = () => import("../views/RinhaInfo");
+const CreateGalo = () => import("../views/CreateGalo");
+const Terms = () => import("../views/Terms");
+const Privacy = () => import("../views/Privacy");
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/terms",
+    name: "Terms",
+    component: Terms,
+  },
+  {
+    path: "/privacy",
+    name: "Privacy",
+    component: Privacy,
   },
   {
     path: "/commands",
     name: "Commands",
-    component: Commands
+    component: Commands,
   },
   {
     path: "/galos",
     name: "Galos",
-    component: Galos
+    component: Galos,
   },
   {
     path: "/donate",
     name: "Donate",
-    component: Donate
+    component: Donate,
   },
   {
     path: "/tutorial",
     name: "RinhaTutorial",
-    component: RinhaTutorial
+    component: RinhaTutorial,
   },
   {
     path: "/info",
     name: "RinhaInfo",
-    component: RinhaInfo
+    component: RinhaInfo,
   },
   {
     path: "/create",
     name: "CreateGalo",
-    component: CreateGalo
-  }
-]
+    component: CreateGalo,
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: "/asura-site",
   routes,
-  scrollBehavior () {
-    return { x: 0, y: 0 }
-  }
-})
+  scrollBehavior() {
+    return { x: 0, y: 0 };
+  },
+});
 
-export default router
+export default router;
