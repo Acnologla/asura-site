7be90ce129892c38965f44ece395ddcedf67e6e7
rafael-jurisoft <rafael@jurisoft.com.br>
2024-12-30 11:07:50 -0300

    Added gtag

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <Nav/>
-    <router-view style="margin-top: 70px"/>
-    <Footer/>
+    <Nav />
+    <router-view style="margin-top: 70px" />
+    <Footer />
   </div>
 </template>
 
@@ -21,7 +21,6 @@ body {
 
 #nav {
   padding: 30px;
-  
 }
 
 #nav a {
@@ -29,32 +28,38 @@ body {
   color: #2c3e50;
 }
 
-.title{
-  color: #4c566a !important
+.title {
+  color: #4c566a !important;
 }
 #nav a.router-link-exact-active {
   color: #42b983;
 }
-[data-theme='dark']{
-    background-color: #2A2D32;
-    color: #ECEFF4;
+[data-theme="dark"] {
+  background-color: #2a2d32;
+  color: #eceff4;
 }
-[data-theme='dark'] #app{
-    color: #ECEFF4;
+[data-theme="dark"] #app {
+  color: #eceff4;
 }
-[data-theme="dark"] .card{
-  background-color: #1D1F21;
-  color: #ECEFF4;
+[data-theme="dark"] .card {
+  background-color: #1d1f21;
+  color: #eceff4;
 }
-[data-theme="dark"] .card-footer{
-  border-top-color: #2A2D32;
+[data-theme="dark"] .card-footer {
+  border-top-color: #2a2d32;
 }
 </style>
 
 <script>
-  import Nav from "./components/Nav"
-  import Footer from "./components/Footer"
-  export default {
-    components: {Nav,Footer}
-  }
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+window.gtag("event", "conversion", {
+  send_to: "AW-11526751589/R_svCOOJj_4ZEOWKsfgq",
+  value: 1.0,
+  currency: "BRL",
+});
+
+export default {
+  components: { Nav, Footer },
+};
 </script>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,44 +1,71 @@
 <template>
- <b-navbar centered  :type="dark ? 'is-dark' : ''" class="nav" style="box-shadow: 0px 0px 20px rgba(0,0,0,0.1);max-width: 100vw;" fixed-top>
-        <template #brand>
-        <b-navbar-item style="margin-left: 30px" tag="router-link" :to="{ name: 'Home' }"  >
-          <span class="navTitle">Asura Bot</span>
-        </b-navbar-item>
-        </template>
-        <template #start>
-            <b-navbar-item href="#about" @click="about" class="navItem">
-                Sobre
-            </b-navbar-item>
-            <b-navbar-item  tag="router-link" :to="{ name: 'Galos' }" class="navItem">
-                    Galos
-            </b-navbar-item>
-            <b-navbar-item tag="router-link" :to="{ name: 'Donate' }"  class="navItem">
-                Doaçoes
-            </b-navbar-item>
-              <b-navbar-item tag="router-link" :to="{ name: 'Commands' }"  class="navItem">
-                Comandos
-            </b-navbar-item>
-            <b-navbar-item tag="a" href="https://top.gg/bot/470684281102925844" target="_blank"  class="navItem">
-                Votar
-            </b-navbar-item>
-        </template>
-        <template #end>
-            <b-navbar-item class="navItem" @click="changeTheme">
-                <font-awesome-icon v-if="dark" icon="sun" />
-                <font-awesome-icon v-else icon="moon" />
-            </b-navbar-item>
-            <b-navbar-item tag="div" id="add" style="margin-right: 30px">
-                <div>
-                    <a target="_blank" href=" https://discordapp.com/oauth2/authorize?client_id=470684281102925844&scope=applications.commands%20bot&permissions=8" class="button is-primary">
-                       <span style="font-weight: 500"> Me adicione</span>
-                    </a>
-                </div>
-            </b-navbar-item>
-        </template>
-      <template slot-scope="{toggleActive}" slot="burger">
-            <a style="margin-top:5px;margin-right: 30px" role="button" @click="toggleActive" aria-label="menu" class= "navbar-burger burger"><span aria-hidden="true"></span><span aria-hidden="true"></span><span aria-hidden="true"></span></a>
-        </template>
-    </b-navbar>
+  <b-navbar
+    centered
+    :type="dark ? 'is-dark' : ''"
+    class="nav"
+    style="box-shadow: 0px 0px 20px rgba(0,0,0,0.1);max-width: 100vw;"
+    fixed-top
+  >
+    <template #brand>
+      <b-navbar-item
+        style="margin-left: 30px"
+        tag="router-link"
+        :to="{ name: 'Home' }"
+      >
+        <span class="navTitle">Asura Bot</span>
+      </b-navbar-item>
+    </template>
+    <template #start>
+      <b-navbar-item href="#about" @click="about" class="navItem">
+        Sobre
+      </b-navbar-item>
+      <b-navbar-item tag="router-link" :to="{ name: 'Galos' }" class="navItem">
+        Galos
+      </b-navbar-item>
+      <b-navbar-item tag="router-link" :to="{ name: 'Donate' }" class="navItem">
+        Doaçoes
+      </b-navbar-item>
+      <b-navbar-item
+        tag="router-link"
+        :to="{ name: 'Commands' }"
+        class="navItem"
+      >
+        Comandos
+      </b-navbar-item>
+      <b-navbar-item
+        tag="a"
+        href="https://top.gg/bot/470684281102925844"
+        target="_blank"
+        class="navItem"
+      >
+        Votar
+      </b-navbar-item>
+    </template>
+    <template #end>
+      <b-navbar-item class="navItem" @click="changeTheme">
+        <font-awesome-icon v-if="dark" icon="sun" />
+        <font-awesome-icon v-else icon="moon" />
+      </b-navbar-item>
+      <b-navbar-item tag="div" id="add" style="margin-right: 30px">
+        <div>
+          <a target="_blank" @click="btn" href="#" class="button is-primary">
+            <span style="font-weight: 500"> Me adicione</span>
+          </a>
+        </div>
+      </b-navbar-item>
+    </template>
+    <template slot-scope="{ toggleActive }" slot="burger">
+      <a
+        style="margin-top:5px;margin-right: 30px"
+        role="button"
+        @click="toggleActive"
+        aria-label="menu"
+        class="navbar-burger burger"
+        ><span aria-hidden="true"></span><span aria-hidden="true"></span
+        ><span aria-hidden="true"></span
+      ></a>
+    </template>
+  </b-navbar>
 </template>
 
 <style>
@@ -47,65 +74,76 @@
     margin-right: 0px !important;
   }
 }
-.navTitle{
-    font-weight: 600;
-    color: rgb(76, 86, 106);
-    font-family: Rubik;
-    font-size: 30px;
+.navTitle {
+  font-weight: 600;
+  color: rgb(76, 86, 106);
+  font-family: Rubik;
+  font-size: 30px;
 }
-.navItem{
-    font-weight: 400;
-    font-family: Rubik;
-    font-size: 20px;
+.navItem {
+  font-weight: 400;
+  font-family: Rubik;
+  font-size: 20px;
 }
-[data-theme="dark"] .navTitle{
-    color: #ECEFF4;
+[data-theme="dark"] .navTitle {
+  color: #eceff4;
 }
-[data-theme="dark"] .navItem{
-    color: #ECEFF4 ;
+[data-theme="dark"] .navItem {
+  color: #eceff4;
 }
-[data-theme="dark"] .navItem:hover{
-    color: grey;
-    background-color: #1D1F21
+[data-theme="dark"] .navItem:hover {
+  color: grey;
+  background-color: #1d1f21;
 }
-[data-theme="dark"] .nav,  [data-theme="dark"] .navbar-menu{
-    background-color: #2A2D32 !important;
+[data-theme="dark"] .nav,
+[data-theme="dark"] .navbar-menu {
+  background-color: #2a2d32 !important;
 }
-
 </style>
 <script>
-export default{
-    name: "Nav",
-    data(){
-        return {
-            dark: document.body.hasAttribute("theme")
-        }
-    },
-    mounted(){
-        if (localStorage.getItem("theme") === "dark"){
-            this.dark = true
-        }
-        this.applyTheme()
-    },
-    methods: {
-        about(){
-            if (this.$route.name !== "Home"){
-                this.$router.push({name: "Home"})
-                document.getElementById('about').scrollIntoView()
-            }
-        },
-        changeTheme(){
-            this.dark
-                ? localStorage.removeItem("theme")
-                : localStorage.setItem("theme", "dark")
-            this.dark = !this.dark
-            this.applyTheme()
-        },
-        applyTheme(){
-            this.dark
-                ? document.body.setAttribute("data-theme", "dark")
-                : document.body.removeAttribute("data-theme")
-        }
+export default {
+  name: "Nav",
+  data() {
+    return {
+      dark: document.body.hasAttribute("theme"),
+    };
+  },
+  mounted() {
+    if (localStorage.getItem("theme") === "dark") {
+      this.dark = true;
     }
-}
+    this.applyTheme();
+  },
+  methods: {
+    about() {
+      if (this.$route.name !== "Home") {
+        this.$router.push({ name: "Home" });
+        document.getElementById("about").scrollIntoView();
+      }
+    },
+    btn() {
+      gtag("event", "conversion", {
+        send_to: "AW-11526751589/-iaDCOaJj_4ZEOWKsfgq",
+        value: 10.0,
+        currency: "BRL",
+      });
+      window.open(
+        "https://discordapp.com/oauth2/authorize?client_id=470684281102925844&scope=applications.commands%20bot&permissions=8",
+        "_blank"
+      );
+    },
+    changeTheme() {
+      this.dark
+        ? localStorage.removeItem("theme")
+        : localStorage.setItem("theme", "dark");
+      this.dark = !this.dark;
+      this.applyTheme();
+    },
+    applyTheme() {
+      this.dark
+        ? document.body.setAttribute("data-theme", "dark")
+        : document.body.removeAttribute("data-theme");
+    },
+  },
+};
 </script>
